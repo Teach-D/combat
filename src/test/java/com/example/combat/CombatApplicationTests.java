4303978d83888c8ed2fbc28d06e7319247113981
wkadh <wkadht0619@gmail.com>
2024-04-08 21:42:58 +0900

    Combat Initial commit

---
+++ src/test/java/com/example/combat/CombatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.combat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
